0d93a9fd3ca68b3f5de51e0f469d844e954688be
msi <meibrohimov.@gmail.com>
2024-01-22 13:55:02 +0500

    Initial commit 📂

---
+++ cursor_object.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+connection.execute('''
+CREATE TABLE users(
+id INTEGER PRIMARY KEY,
+name VARCHAR(255),
+username VARCHAR(255),
+password VARCHAR(9)
+)
+
+''')
+
