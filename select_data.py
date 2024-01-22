8a41b8f036281c318daa61e4fce31c9e8a791e19
msi <meibrohimov.@gmail.com>
2024-01-22 17:10:46 +0500

    select data

--- select_data.py
+++ select_data.py
@@ -6,6 +6,12 @@ cursor = connection.execute("SELECT * FROM users")
 
 for row in cursor:
     print(row)
+# output
+""" 
+(1, 'John', 'john', 1234)
+(2, 'Ali', 'ali', 2222)
+(3, 'Bekzod', 'bek', 4444)
+"""
 
 connection.commit()
 connection.close()
