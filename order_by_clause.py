e8b1e234de91b61fc376e6a96549509afcd6072e
msi <meibrohimov.@gmail.com>
2024-01-23 18:15:36 +0500

    order by clause

---
+++ order_by_clause.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+# create table
+# connection.execute('''CREATE TABLE customer_address
+#          (ID INT PRIMARY KEY     NOT NULL,
+#          NAME           TEXT    NOT NULL,
+#          AGE            INT     NOT NULL,
+#          ADDRESS        CHAR(50)); ''')
+
+
+connection.commit()
+connection.close()
