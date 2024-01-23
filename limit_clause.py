888e78351c0e53e38a84499f13ff17ad5c2d34b4
msi <meibrohimov.@gmail.com>
2024-01-23 18:42:56 +0500

    LIMIT Clause

--- limit_clause.py
+++ limit_clause.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('data.db')
 
-cursor = connection.execute("SELECT * FROM customer_address LIMIT 3")
+cursor = connection.execute("SELECT * FROM customer_address LIMIT 4")
 
 
 for x in cursor:
