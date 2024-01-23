1b2bbc43b3dc05c0c730fdad0eaad3e079cb9a76
msi <meibrohimov.@gmail.com>
2024-01-23 18:40:07 +0500

    order by clause #order_by

--- order_by_clause.py
+++ order_by_clause.py
@@ -7,6 +7,39 @@ connection = sqlite3.connect('data.db')
 #          NAME           TEXT    NOT NULL,
 #          AGE            INT     NOT NULL,
 #          ADDRESS        CHAR(50)); ''')
+# -------------------------------------------------------------
+
+# insert data table
+
+# connection.execute(
+#     "INSERT INTO customer_address VALUES (1, 'nikhil teja', 22, 'hyderabad' )")
+# connection.execute(
+#     "INSERT INTO customer_address VALUES (2, 'karthik', 25, 'khammam')")
+# connection.execute(
+#     "INSERT INTO customer_address VALUES (3, 'sravan', 22, 'ponnur' )")
+# connection.execute(
+#     "INSERT INTO customer_address VALUES (4, 'deepika', 25, 'chebrolu' )")
+# connection.execute(
+#     "INSERT INTO customer_address VALUES (5, 'jyothika', 22, 'noida')")
+# -------------------------------------------------------------
+
+# cursor = connection.execute(
+#     "SELECT ADDRESS,ID from customer_address ORDER BY address DESC")
+#
+# for x in cursor:
+#     print(x)
+
+# -------------------------------------------------------------
+
+# cursor = connection.execute(
+#     "SELECT NAME,ID from customer_address ORDER BY NAME DESC")
+#
+# # display data row by row
+# for i in cursor:
+#     print(i)
+
+# -------------------------------------------------------------
+
 
 
 connection.commit()
