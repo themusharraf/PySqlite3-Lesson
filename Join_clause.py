372a4c5e82e133e07f461fe7afe2cbac50fc9677
msi <meibrohimov.@gmail.com>
2024-01-24 13:41:28 +0500

    Join

---
+++ Join_clause.py
@@ -0,0 +1,18 @@
+"""
+ JOIN bandi umumiy atributlar asosida ikkita jadvaldagi yozuvlarni birlashtiradi. Har xil turdagi ulanishlar quyidagilar:
+
+INNER JOIN (OR JOIN) - ikkala jadvalda umumiy atributlarga ega bo'lgan yozuvlarni beradi.
+LEFT JOIN - chap jadvaldagi barcha yozuvlarni va faqat o'ng jadvaldagi umumiy yozuvlarni beradi.
+RIGHT JOIN - o'ng jadvaldagi barcha yozuvlarni va faqat chap jadvaldagi umumiy yozuvlarni beradi.
+FULL OUTER JOIN - chap yoki o'ng jadvalda umumiy atribut mavjud bo'lganda barcha yozuvlarni beradi.
+CROSS JOIN - bir jadvalning yozuvlarini boshqa jadvalning barcha boshqa yozuvlari bilan birga beradi.
+
+"""  # noqa
+
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+connection.commit()
+connection.close()
+
